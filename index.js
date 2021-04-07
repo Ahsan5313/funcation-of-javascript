7b0431972826d9e5fb66ad1ed9892afaa58ba407
Ahsan5313 <81156921+Ahsan5313@users.noreply.github.com>
2021-04-07 15:42:40 +0600

    add  html and javascript

---
+++ index.js
@@ -0,0 +1,53 @@
+function add(num1,num2){
+
+    add=num1+num2;
+   
+    document.write(" Result ="+add+"<br/>");
+}
+add(5,8);
+
+
+function sub(num1,num2){
+
+    sub=num1-num2;
+   
+    document.write(" Result ="+sub+"<br/>");
+}
+sub(5,8);
+
+
+
+function intto(num1,num2){
+
+    intto=num1*num2;
+   
+    document.write(" Result ="+intto+"<br/>");
+}
+intto(5,8);
+
+function div(num1,num2){
+
+    div=num1/num2;
+   
+    document.write(" Result ="+div+"<br/>");
+}
+div(5,8);
+
+function mul(num1,num2){
+
+    mul=num1%num2;
+   
+    document.write(" Result ="+mul);
+}
+mul(5,8);
+
+
+
+
+
+
+
+
+
+
+
